cb85cef8911388ccdc245cf233878348579840cb
giulange <gyuliano@libero.it>
2015-04-13 10:40:14 +0200

    corrected an IMPORTANT error defining internodal conductivities at top/bottom nodes (tell to A.Coppola)

--- fnsyst.m
+++ fnsyst.m
@@ -1,36 +1,63 @@
 function h2 = fnsyst( P, W )
 % h2 = fnsyst( P, W )
+% 
+% DESCRIPTION
+%   Tridiagonal system
+% 
+% INPUTs
+%   P:      ??
+%   W:      ??
+% 
+% OUTPUTs
+%   h2:     Output potentials at all nodes.
 
 %% init
 h2              = NaN( P.nz, 1);
 AA              = NaN( P.nz, 1);
 BB              = NaN( P.nz, 1);
 CC              = NaN( P.nz, 1);
-%% definire km(i) [è il (ki-1/2)] e il kp(i) [� il (ki+1/2)] 
+kp              = NaN( P.nz, 1);
+km              = NaN( P.nz, 1);
+alpha           = NaN( P.nz, 1);
+beta            = NaN( P.nz, 1);
+gamma           = NaN( P.nz, 1);
+delta           = NaN( P.nz, 1);
+ratio           = NaN( P.nz, 1);
+%% calculate km(i) [i.e. (ki-1/2)] and kp(i) [i.e. (ki+1/2)] 
 
-kp(1,1)         = (P.kond(1) + P.kond(2))/2;
 teta_hbot       = fnteta( W.hbot,  P.sh, P.nz );
 teta_hsurf      = fnteta( W.hsurf, P.sh, 1    );
     
-if or(P.sh.ifc==1,P.sh.ifc==3)
-    kp(P.nz,1)  = ( P.kond(P.nz) + fncond(teta_hbot, P.sh,P.nz) )/2;
-    km(1,1)     = ( P.kond(1)    + fncond(teta_hsurf,P.sh,1   ) )/2;
-else
-    kp(P.nz,1)  = ( P.kond(P.nz) + fncond(W.hbot,P.sh,P.nz    ) )/2;
-    km(1,1)     = ( P.kond(1)    + fncond(W.hsurf,P.sh,1      ) )/2;
+% (ki-1/2) -- top node:
+if or(P.sh.ifc(1)==1,P.sh.ifc(1)==3)
+    km(1,1)     = ( P.kond(1)    + fncond( teta_hsurf,P.sh,1 )      )/2;
+else% 2,4,5
+    km(1,1)     = ( P.kond(1)    + fncond( W.hsurf,P.sh,1 )         )/2;
 end
+% (ki+1/2) -- bottom node:
+if or(P.sh.ifc(P.nz)==1,P.sh.ifc(P.nz)==3)
+    
+    kp(P.nz,1)  = ( P.kond(P.nz) + fncond( teta_hbot, P.sh,P.nz )   )/2;
+else% 2,4,5
+    kp(P.nz,1)  = ( P.kond(P.nz) + fncond( W.hbot,P.sh,P.nz )       )/2;
+    
+end
+% (ki-1/2) -- bottom node:
+km(P.nz,1)      = ( P.kond(P.nz) + P.kond(P.nz-1)       )/2;
+% (ki+1/2) -- top node:
+kp(1,1)         = ( P.kond(1)    + P.kond(2)            )/2;
 
 i               = (2:(P.nz-1));
-km(P.nz,1)      = ( P.kond(P.nz) + P.kond(P.nz-1)        )/2;
-kp(i,1)         = ( P.kond(i)    + P.kond(i+1)           )/2;
-km(i,1)         = ( P.kond(i)    + P.kond(i-1)           )/2;
-
+% (ki-1/2) -- intermediate nodes: (Eq. 2.23 SWAP 32 manual, page 35)
+km(i,1)         = ( P.kond(i)    + P.kond(i-1)          )/2;
+% (ki+1/2) -- intermediate nodes: (Eq. 2.23 SWAP 32 manual, page 35)
+kp(i,1)         = ( P.kond(i)    + P.kond(i+1)          )/2;
 %% risolvere il sistema (vedi swap)
 %% intermediate nodes
 i               = (2:(P.nz-1));
 % d(dx/dz)/dz ==> d(dx/dz1)/dz2
 ratio(i,1)      = W.dtin ./ ( P.nodes.dz(i).^2 );
-alfa(i,1)       = -ratio(i).*km(i);
+alpha(i,1)      = -ratio(i).*km(i);
 beta(i,1)       = P.cap(i) + ratio(i).*km(i) + ratio(i).*kp(i);
 gamma(i,1)      = -ratio(i).*kp(i);
 delta(i,1)      = P.cap(i) .* P.h1(i) + ...
@@ -43,7 +70,7 @@ delta(i,1)      = P.cap(i) .* P.h1(i) + ...
 i               = 1;
 % warning('Antonio wrote i=P.nz-1 (bottom), instead it should be i=1(top)')
 ratio(i,1)      = 2*W.dtin/(P.nodes.dz(i).^2);
-alfa(i,1)       = 0;
+alpha(i,1)      = 0;
 gamma(i,1)      = -ratio(i)*kp(i);
 if W.itbc==0 % itbc differenzia hsurf da qsurf, visto che adesso abbiamo unificato in hqsurf?
     beta(i,1)   = P.cap(i) + ratio(i)*kp(i);
@@ -58,7 +85,7 @@ end
 
 %% bottom node flux
 ratio(P.nz,1)   = 2*W.dtin/(P.nodes.dz(P.nz+1)*P.nodes.dz(P.nz));
-alfa(P.nz,1)    = -ratio(P.nz)*km(P.nz);
+alpha(P.nz,1)   = -ratio(P.nz)*km(P.nz);
 gamma(P.nz,1)   = 0;
 if W.ibbc==0
    beta(P.nz,1) = P.cap(P.nz) + ratio(P.nz)*km(P.nz);
@@ -79,8 +106,8 @@ AA(1)           = delta(1)/beta(1);
 
 % metodo della sostituzione
 for i=2:1:P.nz
-    CC(i)       = beta(i) - alfa(i).*BB(i-1);
-    AA(i)       = ( delta(i) - alfa(i).*AA(i-1) ) ./ CC(i);
+    CC(i)       = beta(i) - alpha(i).*BB(i-1);
+    AA(i)       = ( delta(i) - alpha(i).*AA(i-1) ) ./ CC(i);
     BB(i)       = gamma(i) ./ CC(i);
 end
 
